003ee0333f03a151687d37faceb068e92bb2e89a
arazzik1111 <arazzik1111@gmail.com>
2024-03-29 18:51:41 +0200

    Initial Commit

---
+++ script.py
@@ -0,0 +1,18 @@
+from transformers import pipeline
+
+# Initialize a transformer-based pipeline for question answering
+nlp = pipeline('question-answering')
+
+def answer_question(context, question):
+    result = nlp(question=question, context=context)
+    return result['answer']
+
+
+# Read from a text file
+with open('info.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+# Ask for user input
+question = input("Please enter your question: ")
+
+print(answer_question(text, question))
