8e583297d796640232a9594c2fb530cf795c2244
arazzik1111 <arazzik1111@gmail.com>
2024-05-23 13:49:23 +0300

    UI (Flask) implemented, chatbot interface

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
+from flask import Flask, render_template, request
 from transformers import pipeline
+
+app = Flask(__name__)
 
 # Initialize a transformer-based pipeline for question answering
 nlp = pipeline('question-answering')
@@ -7,13 +10,18 @@ def answer_question(context, question):
     result = nlp(question=question, context=context)
     return result['answer']
 
-
-
 # Read from a text file
 with open('info.txt', 'r', encoding='utf-8') as file:
     text = file.read()
 
-# Ask for user input
-while True:
-    question = input("Please enter your question: ")
-    print(answer_question(text, question))
+@app.route("/")
+def home():    
+    return render_template("index.html") 
+
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(answer_question(text, userText))
+
+if __name__ == "__main__":    
+    app.run()
